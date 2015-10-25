0e783d3a8f4b7035382a12948384d062478b5971
Matan Lieberman <matan.libermann@gmail.com>
2015-10-25 12:04:21 +0200

    Intial commit for package

---
+++ package.js
@@ -0,0 +1,32 @@
+/**
+ * Created by matan on 10/22/15.
+ */
+Package.describe({
+  name: 'bookmd:any-base-converter',
+  version: '0.0.1',
+  summary: 'Util converter. Convert from any string representable base to another',
+  git: ''
+});
+
+Package.onUse(function (api) {
+  api.use([
+    'ecmascript'
+  ]);
+
+  api.addFiles([
+    'lib/baseConversionError.js',
+    'lib/baseConverter.js'
+  ]);
+});
+
+Package.onTest(function (api) {
+  api.use([
+    'ecmascript',
+    'bookmd:any-base-converter@0.0.1',
+    'sanjo:jasmine@0.16.4'
+  ]);
+
+  api.addFiles([
+    'tests/lib/baseConverter.js'
+  ]);
+});
